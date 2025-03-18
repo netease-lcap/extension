e310727a120f67c79f5ef65fde1b0c39572c5834
刘宇阳 <myronliu347@gmail.com>
2025-03-18 13:59:38 +0800

    chore: update tpl

--- packages/create-lcap-extension/templates/react/src/typings.d.ts
+++ packages/create-lcap-extension/templates/react/src/typings.d.ts
@@ -1,3 +1,5 @@
+import '@lcap/builder/typings.d.ts';
+
 declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
@@ -5,40 +7,4 @@ declare module '*.module.css' {
 declare module '*.module.less' {
   const classes: { [key: string]: string };
   export default classes;
-}
-
-namespace nasl.ui {
-  export class BaseEvent {}
-  export class ValidateResult {
-    rawValue: nasl.core.String;
-    value: nasl.core.String;
-    trigger: nasl.core.String;
-    muted: nasl.core.String;
-    valid: nasl.core.Boolean;
-    touched: nasl.core.Boolean;
-    dirty: nasl.core.Boolean;
-    firstError: nasl.core.String;
-  }
-}
-
-namespace nasl.core {
-  export class StringLiteral<T> {
-    _value: T;
-  }
-}
-
-declare namespace extensions {
-  export interface ExtensionComponentOptions {
-    type: 'pc' | 'h5';
-    show?: boolean;
-    replaceNaslUIComponent?: string;
-    extends?: any;
-    sourceName?: string;
-    ideusage: {
-      idetype?: 'element' | 'modal' | 'popover' | 'container' | string;
-      [key: string]: any;
-    };
-  }
-
-  export function ExtensionComponent(options?: ExtensionComponentOptions): (target: any) => void;
 }
